8f6d6cde856df5a870b630fca8294a294277e013
angelawh <angelah@princeton.edu>
2015-06-25 15:06:39 -0400

    Update snapping.js

--- snapping.js
+++ snapping.js
@@ -1,3 +1,6 @@
+/* © 2009 ROBO Design
+ * http://www.robodesign.ro
+ */
 
 // Keep everything in anonymous function, called on window load.
 if(window.addEventListener) {
@@ -6,7 +9,7 @@ window.addEventListener('load', function () {
 
   // The active tool instance.
   var tool;
-  var tool_default = 'line';
+  var tool_default = 'circle';
 
   function init () {
     // Find the canvas element.
@@ -88,9 +91,8 @@ window.addEventListener('load', function () {
   var tools = {};
 
   var objects = [];
-  var cirlces = [];
+  var circles = [];
 
-  // The line tool.
   // The line tool.
   tools.line = function () {
     var tool = this;
@@ -196,10 +198,10 @@ window.addEventListener('load', function () {
         objects.push({type:"circle", coords:[tool.x0, tool.y0]});
       }
     };
-    
   };
 
   init();
 
 }, false); }
 
+// vim:set spell spl=en fo=wan1croql tw=80 ts=2 sw=2 sts=2 sta et ai cin fenc=utf-8 ff=unix:
